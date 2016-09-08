f9d5e699586416a81e32a62d5275b949f8d017b1
Patrickpeng11 <pyh@udel.edu>
2016-09-08 16:25:48 -0400

    Update 2.php

--- 2.php
+++ 2.php
@@ -2,20 +2,20 @@
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
-  $query= "SELECT rack, words FROM racks WHERE length=7 order by random() limit 1";
+    $query= "SELECT rack, words FROM racks WHERE length=7 order by random() limit 1";
 //$query = "SELECT * FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
-$statement = $dbhandle->prepare($query);
+    $statement = $dbhandle->prepare($query);
     $statement->execute();
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
     $a = json_encode($results);
-   echo $a;
+    echo $a;
     $str = substr($a,10,7);
-   $word = substr($a,-10,7);
+    $word = substr($a,-10,7);
 
-   $myrack = $str;
-$racks = [];
+    $myrack = $str;
+    $racks = [];
 for($i = 0; $i < pow(2, strlen($myrack)); $i++){
 	$ans = "";
 	for($j = 0; $j < strlen($myrack); $j++){
@@ -27,13 +27,13 @@ for($i = 0; $i < pow(2, strlen($myrack)); $i++){
   	    $racks[] = $ans;	
 	}
 }
-$racks = array_unique($racks);
+    $racks = array_unique($racks);
 
 
 for ($n=1;$n<=sizeof($racks);$n++){
     
-   $find="SELECT words FROM racks WHERE rack='$racks[$n]'";
-   $statement = $dbhandle->prepare($find);
+    $find="SELECT words FROM racks WHERE rack='$racks[$n]'";
+    $statement = $dbhandle->prepare($find);
     $statement->execute();
     $re = $statement->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
